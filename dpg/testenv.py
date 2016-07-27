30a3d1b7469d8929047f9340c58e34e0aec057b5
--global <frederik.ebert@mytum.de>
2016-07-27 15:41:12 +0200

    small changes

--- dpg/testenv.py
+++ dpg/testenv.py
@@ -1,10 +1,10 @@
 import gym
 import numpy as np
-
-env = gym.make('Reacher-v1')
+import matplotlib.pyplot as plt
+env = gym.make('Acrobot-v0')
 print env.action_space
-print env.action_space.low
-print env.action_space.high
+# print env.action_space.low
+# print env.action_space.high
 
 
 print env.spec.reward_threshold
@@ -17,15 +17,20 @@ for i in range(10000):
 
     sum_rewards = 0
     while not done:
-        action = np.asarray((.001,.0005))
-        # action = env.action_space.sample()
+        # action = np.asarray((.001,.0005))
+        action = env.action_space.sample()
         # state, reward, done, _ = env.step(env.action_space.sample())
         state, reward, done, _ = env.step(action)
         sum_rewards+= reward
-        env.render()
-        print(state)
-        print(done)
+        rgbarray= env.render('rgb_array')
+
+        print('state',state)
+        print('done', done)
         print('reward',reward)
+        print('rgb array size', rgbarray.shape)
+
+        plt.imshow(rgbarray)
+        plt.show()
 
     # print(sum_rewards)
     ep_lengths.append(sum_rewards)
